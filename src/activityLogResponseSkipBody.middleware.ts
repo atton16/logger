ae58e629a086e0670bcf1a7daa28fb83d96c5b51
Attawit Kittikrairit <atton16@gmail.com>
2020-06-26 10:33:11 +0700

    feat(SkipResponseBody): add express-compatible middleware

---
+++ src/activityLogResponseSkipBody.middleware.ts
@@ -0,0 +1,6 @@
+import { Request, Response, } from 'express';
+
+export function activityLogResponseSkipBodyMiddleware(req: Request, res: Response, next: Function): void {
+  res.skipBody = true;
+  next();
+}
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export { logger } from './logger';
 export { activityLogMiddleware } from './activityLog.middleware';
 export { ActivityLogResponseSkipBody } from './activityLogResponseSkipBody.interceptor';
+export { activityLogResponseSkipBodyMiddleware as responseSkipBody } from './activityLogResponseSkipBody.middleware';
