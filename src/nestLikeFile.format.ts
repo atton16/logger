be2e4443a33cf46deca0442dbb45b96705fe70b3
Attawit Kittikrairit <atton16@gmail.com>
2021-03-03 09:33:06 +0700

    patch: add color and timestamp configuration support

--- src/nestLikeFile.format.ts
+++ src/nestLikeFile.format.ts
@@ -1,11 +1,32 @@
 import { Format } from 'logform';
 import { format } from 'winston';
+import bare from 'cli-color/bare';
+import clc from 'cli-color';
 import safeStringify from 'fast-safe-stringify';
 
-export const nestLikeFileFormat = (appName = 'NestWinston'): Format => format.printf(({ context, level, timestamp, message, ...meta }) => {
-  return `${appName} ` +
+const nestLikeColorScheme: Record<string, bare.Format> = {
+  info: clc.greenBright,
+  error: clc.red,
+  warn: clc.yellow,
+  debug: clc.magentaBright,
+  verbose: clc.cyanBright,
+};
+
+export const nestLikeFileFormat = (appName = 'NestWinston', opt = {color: true, timestamp: true}): Format => format.printf(({ context, level, timestamp, message, ...meta }) => {
+  const color = nestLikeColorScheme[level] || ((text: string): string => text);
+
+  if (opt.color) {
+    return `${color(`[${appName}]`)} ` +
+           `${clc.yellow(level.charAt(0).toUpperCase() + level.slice(1))}\t` +
+           ('undefined' !== typeof timestamp && opt.timestamp ? `${new Date(timestamp).toLocaleString()} ` : '') +
+           ('undefined' !== typeof context ? `${clc.yellow('[' + context + ']')} ` : '') +
+           `${color(message)} - ` +
+           `${safeStringify(meta)}`;
+  }
+
+  return `[${appName}] ` +
          `${level.charAt(0).toUpperCase() + level.slice(1)}\t` +
-         ('undefined' !== typeof timestamp ? `${new Date(timestamp).toLocaleString()} ` : '') +
+         ('undefined' !== typeof timestamp && opt.timestamp ? `${new Date(timestamp).toLocaleString()} ` : '') +
          ('undefined' !== typeof context ? `${'[' + context + ']'} ` : '') +
          `${message} - ` +
          `${safeStringify(meta)}`;
